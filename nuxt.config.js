e558cf84aa1546752744ae7b9cef71f340d5403f
kaiiy <81578246+kaiiy@users.noreply.github.com>
2023-05-23 22:02:58 +0900

    wip: 2023-05-23 22:02:58+09:00

--- nuxt.config.js
+++ nuxt.config.js
@@ -80,9 +80,9 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
-  // router: {
-  //   base: '/c-lock-row/crowd/',
-  // },
+  router: {
+    base: '/crowd/',
+  },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['plugins/vuetify'],
