711ad079cf9c9ec8592e0d94c2cfd64f554a0aa3
kaiiy <81578246+kaiiy@users.noreply.github.com>
2024-02-28 12:27:10 +0000

    Update script lang attribute to "ts" and remove unused imports

--- pages/index.vue
+++ pages/index.vue
@@ -8,21 +8,13 @@
   </Canvas>
 </template >
     
-<script lang=" ts">
+<script lang="ts">
 import { defineComponent } from '@vue/composition-api'
-import Thumbnail from '~/components/specific/list/thumbnail/Thumbnail.vue'
 import HeaderTitle from '~/components/specific/list/TheHeaderTitle.vue';
 import Canvas from '~/components/specific/list/TheCanvas.vue';
 import ThumbnailsWrap from '~/components/specific/list/thumbnail/TheThumbnailsWrap.vue';
 
-import { comazeInfo } from '~/assets/games/kaii/comaze/settings';
-import { unidentifiedInfo } from '~/assets/games/kaii/unidentified/settings';
-import { lunarBalanceInfo } from '~/assets/games/kaii/lunar-balance/settings';
-
 export default defineComponent({
-  setup() {
-    return { comazeInfo, unidentifiedInfo, lunarBalanceInfo };
-  },
-  components: { Thumbnail, HeaderTitle, Canvas, ThumbnailsWrap }
+  components: { HeaderTitle, Canvas, ThumbnailsWrap }
 })
 </script>
